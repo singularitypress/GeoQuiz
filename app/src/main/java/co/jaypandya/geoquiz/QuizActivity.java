b35bd75184692add90fa13068c5a52766e52833b
singularitypress <jaypandya2k5@gmail.com>
2016-02-15 21:42:38 -0500

    fixed orientation layout. Need to fix reset bug.

--- app/src/main/java/co/jaypandya/geoquiz/QuizActivity.java
+++ app/src/main/java/co/jaypandya/geoquiz/QuizActivity.java
@@ -1,8 +1,6 @@
 package co.jaypandya.geoquiz;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /*
@@ -185,6 +184,24 @@ public class QuizActivity extends AppCompatActivity {
     public void onPause(){
         super.onPause();
         Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
